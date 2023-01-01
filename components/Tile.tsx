7d75e9281ff3b5d6766a71140fd2ec4e13d94ec0
sol-cryptokid <thomas.bazire@icloud.com>
2023-01-01 17:27:55 +0100

    Adding sample tile component

---
+++ components/Tile.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from "react"
+
+interface TileProps {
+    children: ReactElement | string | null | undefined,
+    title?: string | null | undefined
+}
+
+export default function Tile ({ children, title } : TileProps) {
+    return (
+        <div className="bg-[#d9d9d9]/10 rounded-lg py-4 px-6">
+            <h3 className="text-white text-lg font-semibold">{ title }</h3>
+            { children }
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react"
+import Tile from "../components/Tile"
 import { useWeb3Context } from "../contexts/Web3"
 import { useWeb3 } from "../hooks/Web3"
 
@@ -6,13 +7,18 @@ import { useWeb3 } from "../hooks/Web3"
 export default function HomePage ()  {
   const { address } = useWeb3Context()
 
-  useEffect(() => {
-    console.log(`Got new address triggered: ${address}`)
-  }, [address])
-
   return (
-    <div className="h-screen w-full bg-[#000F11] justify-center">
-      <h1 className="text-3xl font-semibold text-center text-white">Hello there!</h1>
+    <div className="container mx-auto">
+      <div className="flex my-10">
+        <div className="flex-1">
+
+        </div>
+        <div className="flex-1 pl-12 ml-24">
+          <Tile title="Net worth">
+            42,454€
+          </Tile>
+        </div>
+      </div>
       <span className="text-white text-sm font-mono">Addresss: { address }</span>
     </div>
   )
